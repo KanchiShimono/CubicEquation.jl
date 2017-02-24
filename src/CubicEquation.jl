277a7806e495d7a8768b95421de84b11dcdb9fb4
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2017-02-25 02:13:04 +0900

    Divide solving process depending on the discriminant

--- src/CubicEquation.jl
+++ src/CubicEquation.jl
@@ -15,29 +15,38 @@ function (solver::Solver)(a::Real, b::Real, c::Real, d::Real)
     h = g^2 / 4 + f^3 / 27
     if f == 0.0 && g == 0.0 && h == 0.0
         x1 = x2 = x3 = -cbrt(d/a)
+        return [x1, x2, x3]
     elseif  h <= 0.0
-        i = (g^2 / 4 - h)^(1/2)
-        j = cbrt(i)
-        K = acos(-(g/2i))
-        L = -j
-        M = cos(K/3)
-        N = sqrt(3) * sin(K/3)
-        P = -(b/3a)
-        x1 = 2j * cos(K/3) - (b/3a)
-        x2 = L * (M + N) + P
-        x3 = L * (M - N) + P
+        return three_real_root(a, b, g, h)
     elseif h > 0.0
-        i = sqrt(complex((g^2 / 4 - h)))
-        R = -g/2 + sqrt(h)
-        S = cbrt(R)
-        T = -g/2 - sqrt(h)
-        U = cbrt(T)
-        x1 = (S + U) - (b/3a)
-        x2 = -(S + U)/2 - (b/3a) + im*(S-U)*sqrt(3) / 2
-        x3 = -(S + U)/2 - (b/3a) - im*(S-U)*sqrt(3) / 2
+        return one_real_root(a, b, g, h)
     end
 
-    return x1, x2, x3
+end
+
+function three_real_root(a::Real, b::Real, g::Float64, h::Float64)
+    i = (g^2 / 4 - h)^(1/2)
+    j = cbrt(i)
+    K = acos(-(g/2i))
+    L = -j
+    M = cos(K/3)
+    N = sqrt(3) * sin(K/3)
+    P = -(b/3a)
+    x1 = 2j * cos(K/3) - (b/3a)
+    x2 = L * (M + N) + P
+    x3 = L * (M - N) + P
+    return sort([x1, x2, x3])
+end
+
+function one_real_root(a::Real, b::Real, g::Float64, h::Float64)
+    R = -g/2 + sqrt(h)
+    S = cbrt(R)
+    T = -g/2 - sqrt(h)
+    U = cbrt(T)
+    x1 = (S + U) - (b/3a)
+    x2 = -(S + U)/2 - (b/3a) - im*(S-U)*sqrt(3) / 2
+    x3 = -(S + U)/2 - (b/3a) + im*(S-U)*sqrt(3) / 2
+    return [x1, x2, x3]
 end
 
 end #end module
