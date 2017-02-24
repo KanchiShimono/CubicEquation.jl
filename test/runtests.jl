beb61a2dbf799f27d56e2a9e9ff34a717d018df3
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2017-02-25 04:02:49 +0900

    Improve runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,11 @@ using CubicEquation
 using Base.Test
 
 @testset "solve" begin
-    println("Testing solver...")
+    Base.info("Testing solver...")
     solver = Solver()
     @test isapprox(solver(1.0,-3.0,3.0,-1.0), ones(3))
     @test isapprox(solver(2.0,-3.0,-5.0,6.0), [-1.5,1.0,2.0])
     @test isapprox(solver(1.0,1.0,1.0,-3.0), [1.0,-1.0-im*√(2),-1.0+im*√(2)])
-    println("Done testing solver.")
+    @test_throws ErrorException solver(0.0,1.0,1.0,-3.0)
+    Base.info("Done testing solver.")
 end
