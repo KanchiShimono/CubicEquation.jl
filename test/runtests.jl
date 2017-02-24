b38e0d4329132b584143b657815c85669d179a6f
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2017-02-25 02:14:21 +0900

    Improve runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ using Base.Test
 @testset "solve" begin
     println("Testing solver...")
     solver = Solver()
-    @test isapprox(collect(solver(2.0,-3.0,-5.0,6.0)), collect((2.0,-1.5,1.0)))
+    @test isapprox(solver(1.0,-3.0,3.0,-1.0), ones(3))
+    @test isapprox(solver(2.0,-3.0,-5.0,6.0), [-1.5,1.0,2.0])
+    @test isapprox(solver(1.0,1.0,1.0,-3.0), [1.0,-1.0-im*√(2),-1.0+im*√(2)])
     println("Done testing solver.")
 end
